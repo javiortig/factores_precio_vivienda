e3780fff8a1f39097c9f1debfb00d4db9147f1e8
Soyunservivo <javiortioreti123@gmail.com>
2024-10-22 23:10:27 +0200

    primer commit

---
+++ explorar/README.md
@@ -0,0 +1,4 @@
+# Análisis de Factores que Impactan en los Precios de Vivienda en España y posibles predicciones de sus futuros precios
+
+En este trabajo se busca realizar una minería de datos de distintas fuentes de datos que puedan ser relevantes para
+posteriormente realizar un análisis de las posibles características que afectan con mayor intensidad al precio de la vivienda en España.
---
+++ explorar/extraer_idealista.py
@@ -0,0 +1,57 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+"""
+Este método puede ser limitante debido a las medidas de ciberseguridad que tiene idealista, ya que no permite hacer vairas peticiones 
+de forma continuada. Quizá sería interesante utilizar proxies rotativos.
+"""
+
+url = 'https://www.idealista.com/en/venta-viviendas/madrid-madrid/'
+
+# Headers para imitar los requests de un browser normal
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'DNT': '1',
+    'Referer': 'https://www.google.com/',  # imita a google
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'TE': 'Trailers',
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    listings = soup.find_all('article', class_='item')
+
+    properties = []
+
+     # Extrae cada propiedad
+    for listing in listings:
+        title = listing.find('a', class_='item-link').get_text(strip=True)
+        price = listing.find('span', class_='item-price').get_text(strip=True)
+        location = listing.find('span', class_='item-detail').get_text(strip=True)
+        
+        properties.append([title, price, location])
+        
+        print(f'Título: {title}')
+        print(f'Precio: {price}')
+        print(f'Localización: {location}')
+        print('-' * 50)
+
+    # Guarda los datos en un csv
+    with open('idealista_listings.csv', mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(['Title', 'Price', 'Location'])
+
+        writer.writerows(properties)
+else:
+    print('Failed to retrieve the webpage. Status code:', response.status_code)
